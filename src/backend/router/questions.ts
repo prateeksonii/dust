1b71a9e682ecda15b92716ff0292a3c6ca2d5428
Prateek Soni <prateek_soni@outlook.com>
2022-06-16 00:48:17 +0530

    add inter, add mutation
    Signed-off-by: Prateek Soni <prateek_soni@outlook.com>

--- src/backend/router/questions.ts
+++ src/backend/router/questions.ts
@@ -1,8 +1,23 @@
 import * as trpc from "@trpc/server";
 import { prisma } from "../../db/client";
+import { z } from "zod";
 
-export const questionsRouter = trpc.router().query("get-all", {
-  async resolve() {
-    return await prisma.pollQuestion.findMany();
-  },
-});
+export const questionsRouter = trpc
+  .router()
+  .query("get-all", {
+    async resolve() {
+      return await prisma.pollQuestion.findMany();
+    },
+  })
+  .mutation("create", {
+    input: z.object({
+      question: z.string().min(5).max(400),
+    }),
+    async resolve({ input }) {
+      return await prisma.pollQuestion.create({
+        data: {
+          question: input.question,
+        },
+      });
+    },
+  });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,31 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import { trpc } from "../utils/trpc";
+
+const QuestionCreator: React.FC = () => {
+  const { mutate } = trpc.useMutation("questions.create");
+
+  return (
+    <input
+      type="text"
+      onSubmit={(event) => {
+        console.log(event.currentTarget.value);
+      }}
+    />
+  );
+};
 
 const Home: NextPage = () => {
   const { data, isLoading } = trpc.useQuery(["questions.get-all"]);
 
   if (isLoading || !data) return <div>Loading...</div>;
 
-  console.log(data);
-
-  return <div>{data[0]?.question}</div>;
+  return (
+    <div className="flex flex-col p-8">
+      <h1 className="text-4xl font-extrabold tracking-tight">Questions:</h1>
+      <div>{data[0]?.question}</div>
+      <QuestionCreator />
+    </div>
+  );
 };
 
 export default Home;
